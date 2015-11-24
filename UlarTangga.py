8172930b639fb7591462b00527620fe37e0f6289
JohanesKalas <kalaskinasah@gmail.com>
2015-11-24 11:44:32 +0700

    Update UlarTangga.py

--- UlarTangga.py
+++ UlarTangga.py
@@ -1,1 +1,28 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
 
+__author__ = "Johanes Kalas, Carissa Ulibasa, Devi Apriliani S, Jovanka Helen, Reza Haznam"
+             
+class Player:
+	warna = ''
+	mataDadu = 0
+	posisi = 0
+	def __init__(self, warna):
+		self.warna = warna
+	def getWarna(self):
+		return self.warna
+	def setPosition(self,x):
+		self.posisi = x
+	def getPosition(self):
+		return self.posisi
+
+import random
+
+class Dice:
+	mataDadu = 0
+	def __init__(self):
+		self.mataDadu = 0
+	def getMataDadu(self):
+                self.mataDadu = random.randint(1,6)
+                return self.mataDadu
